37b7a18edb0ffd08fc2384261671add922d494ee
hzy46 <362583303@qq.com>
2020-02-19 12:25:07 +0800

    fix

--- src/kube-runtime/build/build-pre.sh
+++ src/kube-runtime/build/build-pre.sh
@@ -23,18 +23,47 @@ if [ -d "../dependency" ]; then
     rm -rf "../dependency"
 fi
 
-mkdir -p "../dependency"
+mkdir -p "../dependency/package-cache"
+ROOT_DIR=$(cd "../dependency/package-cache"; pwd)
+
+if hash sudo 2>/dev/null; then
+  # if sudo exists, use `sudo docker` instead of `docker`.
+  alias docker='sudo docker'
+fi
 
 while IFS= read -r line || [[ -n "$line" ]] ;
 do
     start_char=`echo $line | cut -b 1`
-    if [ ! "$start_char" = "#" ]; then
+    if [ ! "$start_char" == "#" ]; then
       name=`echo $line | cut -d , -f 1`
       os=`echo $line | cut -d , -f 2`
       packages=`echo $line | cut -d , -f 3`
       precommands=`echo $line | cut -d , -f 4`
       echo "name: ${name} os: ${os} packages: ${packages}"
-    fi    
+    fi
+    if [ "$os" == "ubuntu16.04" -o "$os" == "ubuntu18.04" ]; then
+      if [ "$os" == "ubuntu16.04" ]; then
+        base_image="ubuntu:16.04"
+      fi
+      if [ "$os" == "ubuntu18.04" ]; then
+        base_image="ubuntu:18.04"
+      fi
+      package_dir=$ROOT_DIR"/${name}-${os}"
+      mkdir -p $package_dir
+      docker run -i -v $package_dir:/mount $base_image -e PACKAGES=${packages} \
+        /bin/bash  <<- EOF_DOCKER
+                   apt-get update
+                   apt-get -y install --print-uris $PACKAGES | cut -d " " -f 1-2 | grep http:// > /aptinfo && \
+                   cat /aptinfo | cut -d\' -f 2 > /apturl && \
+                   apt-get -y install wget && \
+                   wget -i /apturl --tries 3 -P /mount && \
+                   cat /aptinfo | cut -d " " -f 2 > /mount/order
+                   EOF_DOCKER
+    else
+      echo "Only os=ubuntu16.04 or os=ubuntu18.04 is supported! Found: $os"
+      exit 1
+    fi
+
 done < "package-cache-info"
 
 echo "hello" > "../dependency/x"
